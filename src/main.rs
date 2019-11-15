b7cc2f692041e23eb87b3b6dcb15cbd49596f4f7
Heather Moore <moore.hj@gmail.com>
2019-11-15 20:30:51 +0100

    add scripting game to guess a number

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    let mut try_count = 0;
+
+    loop {
+        println!("Guess a number between 1 and 100:");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => {
+                try_count = try_count + 1;
+                num
+            }
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+
+    println!("You got it in {} guesses.", try_count);
+}
